e727b71d487759a6ce3971db7e7950b9d1044ab6
ericjbergan <ericjbergan@gmail.com>
2022-07-06 19:35:45 -0500

    working on bride guest info js

--- Application/src/main/java/com/kenzie/appserver/controller/RsvpController.java
+++ Application/src/main/java/com/kenzie/appserver/controller/RsvpController.java
@@ -107,7 +107,7 @@ public class RsvpController {
     public ResponseEntity<RsvpResponse> updateRsvp(@RequestBody RsvpCreateRequest rsvpCreateRequest) {
         RsvpRecord rsvpRecord = rsvpService.findByName(rsvpCreateRequest.getName());
         rsvpRecord.setName(rsvpCreateRequest.getName());
-        rsvpRecord.setAttending(rsvpCreateRequest.isAttending());
+        rsvpRecord.setAttending(rsvpCreateRequest.getIsAttending());
         rsvpRecord.setMealChoice(rsvpCreateRequest.getMealChoice());
         rsvpRecord.setPlus1Name(rsvpCreateRequest.getPlus1Name());
         rsvpRecord.setPlus1MealChoice(rsvpCreateRequest.getPlus1MealChoice());
@@ -137,7 +137,7 @@ public class RsvpController {
         RsvpResponse rsvpResponse = new RsvpResponse();
         rsvpResponse.setName(rsvpCreateRequest.getName());
         rsvpResponse.setEmail(rsvpCreateRequest.getEmail());
-        rsvpResponse.setAttending(rsvpCreateRequest.isAttending());
+        rsvpResponse.setAttending(rsvpCreateRequest.getIsAttending());
         rsvpResponse.setMealChoice(rsvpCreateRequest.getMealChoice());
         rsvpResponse.setPlus1Name(rsvpCreateRequest.getPlus1Name());
         rsvpResponse.setPlus1MealChoice(rsvpCreateRequest.getPlus1MealChoice());
